d52be029b5a69f54db4d84f63a53a63c85f1e74f
Phuong Cao <caongocphuong95@gmail.com>
2024-01-13 10:20:13 +0700

    git init

---
+++ QuanLyKho/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace QuanLyKho.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            MessageBox.Show("Đã vô Main View Model - la Data context cua Main windown xaml");
+        }
+    }
+}
